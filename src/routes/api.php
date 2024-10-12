0ccfa4bb5fe4223733c82f876fdab3fb6903096a
seolyam <leeyam.dev@gmail.com>
2024-10-12 15:11:50 +0800

    Added /test-db route to test database connection

--- src/routes/api.php
+++ src/routes/api.php
@@ -1,5 +1,23 @@
+<?php
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+use Illuminate\Database\Capsule\Manager as Capsule;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = AppFactory::create();
+
 $app->get('/test-db', function (Request $request, Response $response, $args) {
-    $users = Capsule::table('products')->get(); // Just to see if it fetches data
-    $response->getBody()->write($users);
+    try {
+        $products = Capsule::table('products')->get(); // Assuming you have a 'products' table
+        $response->getBody()->write($products->toJson());
+    } catch (Exception $e) {
+        $response->getBody()->write("Database connection failed: " . $e->getMessage());
+    }
+
     return $response;
 });
+
+$app->run();
