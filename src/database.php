c11c6d4bc21620bc1b8bd8ecf04d468950fccb01
seolyam <leeyam.dev@gmail.com>
2024-10-12 17:25:13 +0800

    PLEASE WORK IM BEGGING

--- src/database.php
+++ src/database.php
@@ -2,15 +2,21 @@
 
 use Illuminate\Database\Capsule\Manager as Capsule;
 
+$host = getenv('MYSQLHOST');
+$port = getenv('MYSQLPORT');
+$database = getenv('MYSQLDATABASE');
+$username = getenv('MYSQLUSER');
+$password = getenv('MYSQLPASSWORD');
+
 $capsule = new Capsule;
 
 $capsule->addConnection([
     'driver'    => 'mysql',
-    'host'      => getenv('MYSQLHOST'),
-    'port'      => getenv('MYSQLPORT'),
-    'database'  => getenv('MYSQLDATABASE'),
-    'username'  => getenv('MYSQLUSER'),
-    'password'  => getenv('MYSQLPASSWORD'),
+    'host'      => $host,
+    'port'      => $port,
+    'database'  => $database,
+    'username'  => $username,
+    'password'  => $password,
     'charset'   => 'utf8',
     'collation' => 'utf8_unicode_ci',
     'prefix'    => '',
