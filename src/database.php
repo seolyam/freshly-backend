b9b86884bb95c0807bc77e748be414a268866eb3
seolyam <leeyam.dev@gmail.com>
2024-10-12 15:49:27 +0800

    Configured MySQL database connection

--- src/database.php
+++ src/database.php
@@ -8,11 +8,11 @@ $capsule = new Capsule;
 
 $capsule->addConnection([
     'driver'    => 'mysql',
-    'host'      => getenv('MYSQLHOST'),
-    'port'      => getenv('MYSQLPORT'),
-    'database'  => getenv('MYSQLDATABASE'),
-    'username'  => getenv('MYSQLUSER'),
-    'password'  => getenv('MYSQLPASSWORD'),
+    'host'      => getenv('DB_HOST'),
+    'port'      => getenv('DB_PORT'),
+    'database'  => getenv('DB_NAME'),
+    'username'  => getenv('DB_USER'),
+    'password'  => getenv('DB_PASS'),
     'charset'   => 'utf8',
     'collation' => 'utf8_unicode_ci',
     'prefix'    => '',
