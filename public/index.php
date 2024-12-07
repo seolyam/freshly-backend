06e09dfdafbb2e2c462db2803803e01327bef47c
seolyam <leeyam.dev@gmail.com>
2024-12-07 21:11:37 +0800

    fix: add cart to route

--- public/index.php
+++ public/index.php
@@ -12,49 +12,67 @@ use App\TursoClient;
 use App\Repository\UserExtraRepository;
 use Dotenv\Dotenv;
 
+// Load environment variables
 if (file_exists(__DIR__ . '/../.env')) {
     $dotenv = Dotenv::createImmutable(__DIR__ . '/../');
     $dotenv->load();
 }
 
+// Validate database environment variables
 $databaseUrl = $_ENV['TURSO_DB_URL'] ?? '';
 $authToken = $_ENV['TURSO_AUTH_TOKEN'] ?? '';
 if (empty($databaseUrl) || empty($authToken)) {
     throw new \Exception('Database URL or Auth Token is not set in the environment variables.');
 }
 
+// Initialize Turso client
 $tursoClient = new TursoClient($databaseUrl, $authToken);
 
+// Create Slim app
 $app = AppFactory::create();
-$app->addRoutingMiddleware(); 
+$app->addRoutingMiddleware();
 $app->addBodyParsingMiddleware();
 
+// Controllers
 $productController = new ProductController($tursoClient);
 $userController = new UserController();
+$cartController = new CartController($tursoClient);
 
 // Instantiate UserExtraController
 $extraRepo = new UserExtraRepository($tursoClient);
 $userExtraController = new UserExtraController($extraRepo);
 
 // Products routes
-$app->get('/products', [$productController, 'getAllProducts']);
-$app->get('/products/{id}', [$productController, 'getProductById']);
-$app->post('/products', [$productController, 'createProduct']);
-$app->put('/products/{id}', [$productController, 'updateProduct']);
-$app->delete('/products/{id}', [$productController, 'deleteProduct']);
+$app->group('/products', function ($group) use ($productController) {
+    $group->get('', [$productController, 'getAllProducts']);
+    $group->get('/{id}', [$productController, 'getProductById']);
+    $group->post('', [$productController, 'createProduct']);
+    $group->put('/{id}', [$productController, 'updateProduct']);
+    $group->delete('/{id}', [$productController, 'deleteProduct']);
+});
 
 // User routes
-$app->post('/register', [$userController, 'register']);
-$app->post('/login', [$userController, 'login']);
-$app->get('/profile', [$userController, 'getProfile'])->add(new AuthMiddleware());
-$app->post('/profile', [$userController, 'updateProfile'])->add(new AuthMiddleware());
-$app->post('/cart', [CartController::class, 'addCartItem'])->add(new AuthMiddleware());
+$app->group('/user', function ($group) use ($userController) {
+    $group->post('/register', [$userController, 'register']);
+    $group->post('/login', [$userController, 'login']);
+    $group->get('/profile', [$userController, 'getProfile'])->add(new AuthMiddleware());
+    $group->post('/profile', [$userController, 'updateProfile'])->add(new AuthMiddleware());
+});
+
+// Cart routes
+$app->group('/cart', function ($group) use ($cartController) {
+    $group->post('', [$cartController, 'addCartItem'])->add(new AuthMiddleware());
+    $group->post('/add', [$cartController, 'addCartItem'])->add(new AuthMiddleware());
+    $group->get('', [$cartController, 'getCartItems'])->add(new AuthMiddleware());
+});
 
 // Extra user info route (for contactNumber, address, birthdate)
 $app->post('/update-user-info', [$userExtraController, 'updateUserInfo'])->add(new AuthMiddleware());
 
+// Add error middleware for debugging
 $app->addErrorMiddleware(true, true, true);
 
+// Test route for debugging purposes
 $app->get('/test-products', function ($request, $response) use ($tursoClient) {
     $sql = 'SELECT * FROM products';
     $productResult = $tursoClient->executeQuery($sql);
@@ -62,4 +80,5 @@ $app->get('/test-products', function ($request, $response) use ($tursoClient) {
     return $response->withHeader('Content-Type', 'application/json')->withStatus(200);
 });
 
+// Run the app
 $app->run();
